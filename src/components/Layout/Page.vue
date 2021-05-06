0534cc611fbe465a161bc4b811def5ea84613c80
picory <drawhalf@nate.com>
2021-05-06 21:30:59 +0900

    commit 2nd

---
+++ src/components/Layout/Page.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                <h1>Layout Page</h1>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class LayoutPage extends Vue {}
+</script>
+
+<style scoped></style>
